e130acf22e5265affcdb8d7b7fbdaa42889b9016
sushil sigdel <62206321+sushil35278@users.noreply.github.com>
2023-03-07 21:47:57 +0545

    new commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from webcolors import name_to_hex
+
+def color_name_to_code(color_name):
+  try:
+    color_code = name_to_hex(color_name)
+    return color_code
+  except ValueError:
+    return None
+colorname = input("Enter color name: ")
+result_code = color_to_code(colorname)
+print(result_code)
